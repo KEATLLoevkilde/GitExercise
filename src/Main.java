3ca84d1bb51f3df85d77bb85ff2c10aab057a16d
KEATTLoevkilde <thom4a14@stud.kea.dk>
2022-09-09 10:05:20 +0200

    added greet

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         SuperGreeter superGreeter = new SuperGreeter();
         superGreeter.start();
+        superGreeter.greet();
     }
 }
--- src/SuperGreeter.java
+++ src/SuperGreeter.java
@@ -1,5 +1,15 @@
+import java.util.Scanner;
+
 public class SuperGreeter {
     public void start(){
         System.out.println("Det virker!");
     }
+    public void greet (){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Intast navn:");
+        String navn = sc.next();
+        System.out.println("Hej" + navn);
+    }
+
+
 }
